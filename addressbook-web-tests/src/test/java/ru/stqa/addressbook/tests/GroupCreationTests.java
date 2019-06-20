a6c0fdf57b1770b570cca1fee22c3ac8566ebbfe
svo216 <svo.hljott.216@gmail.com>
2019-06-20 15:52:41 +0500

    4.8. поиск максимального элемента в коллекции (группы)

--- addressbook-web-tests/src/test/java/ru/stqa/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/addressbook/tests/GroupCreationTests.java
@@ -3,6 +3,8 @@ package ru.stqa.addressbook.tests;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.addressbook.model.GroupData;
+
+import java.util.HashSet;
 import java.util.List;
 
 
@@ -12,9 +14,20 @@ public class GroupCreationTests extends TestBase {
     public void testGroupCreation() throws Exception {
         app.getNavigationHelper().gotoGroupPage();
         List<GroupData> before = app.getGroupHelper().getGroupList();
-        app.getGroupHelper().createGroup(new GroupData("New", "header", "footer"));
+        GroupData group = new GroupData("New1", null, null);
+        app.getGroupHelper().createGroup(group);
         List<GroupData> after = app.getGroupHelper().getGroupList();
         Assert.assertEquals(after.size(), before.size() + 1);
+
+        int max = 0;
+        for (GroupData g : after) {
+            if (g.getId() > max) {
+                max = g.getId();
+            }
+        }
+        group.setId(max);
+        before.add(group);
+        Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
 
     }
 
