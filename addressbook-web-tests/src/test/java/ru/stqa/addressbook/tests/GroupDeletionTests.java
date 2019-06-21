747b8d513e1109260f44af7ff6dd84c2635ca85e
svo216 <svo.hljott.216@gmail.com>
2019-06-21 11:58:18 +0500

    реализовать проверки путём сравнения списков в тестах для удаления групп

--- addressbook-web-tests/src/test/java/ru/stqa/addressbook/tests/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/addressbook/tests/GroupDeletionTests.java
@@ -4,6 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.addressbook.model.GroupData;
 
+import java.util.Comparator;
 import java.util.List;
 
 public class GroupDeletionTests extends TestBase {
@@ -16,14 +17,17 @@ public class GroupDeletionTests extends TestBase {
       app.getNavigationHelper().gotoGroupPage();
     }
     List<GroupData> before = app.getGroupHelper().getGroupList();
+    GroupData group = new GroupData("New2", null, null);
     app.getGroupHelper().selectGroup(before.size() - 1);
     app.getGroupHelper().deleteSelectedGroup();
-    app.getGroupHelper().returnToGroupPage();
+    app.getNavigationHelper().gotoGroupPage();
     List<GroupData> after = app.getGroupHelper().getGroupList();
     Assert.assertEquals(after.size(), before.size() - 1);
-
     before.remove(before.size() - 1);
-    Assert.assertEquals(before, after);
+    Comparator<? super GroupData> byId = (g1, g2) -> Integer.compare(g1.getId(), g2.getId());
+    before.sort(byId);
+    after.sort(byId);
+    Assert.assertEquals(before,after);
   }
 
 }
