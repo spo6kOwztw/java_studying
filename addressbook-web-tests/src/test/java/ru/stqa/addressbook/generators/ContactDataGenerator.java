456bcd60bbf551efe352ebac084f6064884c544e
svo216 <svo.hljott.216@gmail.com>
2019-08-20 11:57:12 +0500

    реализован генератор json для контактов

--- addressbook-web-tests/src/test/java/ru/stqa/addressbook/generators/ContactDataGenerator.java
+++ addressbook-web-tests/src/test/java/ru/stqa/addressbook/generators/ContactDataGenerator.java
@@ -3,6 +3,8 @@ package ru.stqa.addressbook.generators;
 import com.beust.jcommander.JCommander;
 import com.beust.jcommander.Parameter;
 import com.beust.jcommander.ParameterException;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.thoughtworks.xstream.XStream;
 import ru.stqa.addressbook.model.ContactData;
 
@@ -70,12 +72,21 @@ public class ContactDataGenerator {
             saveAsCsv(contacts, new File(file));
         } else if (format.equals("xml")) {
             saveAsXml(contacts, new File(file));
-        } else {
+        } else if (format.equals("json")){
+            saveAsJson(contacts, new File(file));
             System.out.println("Unrecognized format " + format);
         }
 
     }
 
+    private void saveAsJson(List<ContactData> contacts, File file) throws IOException  {
+        Gson gson =  new GsonBuilder().setPrettyPrinting().excludeFieldsWithoutExposeAnnotation().create();
+        String json = gson.toJson(contacts);
+        try (Writer writer = new FileWriter(file)) {
+            writer.write(json);
+        }
+
+    }
     private void saveAsXml(List<ContactData> groups, File file) throws IOException {
         XStream xstream = new XStream();
         xstream.processAnnotations(ContactData.class);
--- addressbook-web-tests/src/test/java/ru/stqa/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/addressbook/tests/ContactCreationTests.java
@@ -1,5 +1,7 @@
 package ru.stqa.addressbook.tests;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import com.thoughtworks.xstream.XStream;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
@@ -8,7 +10,6 @@ import ru.stqa.addressbook.model.Contacts;
 
 import java.io.*;
 
-import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -19,8 +20,8 @@ import static org.hamcrest.MatcherAssert.assertThat;
 public class ContactCreationTests extends TestBase {
 
     @DataProvider
-    public Iterator<Object[]> validContacts() throws IOException {
-        BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/contacts.xml")));
+    public Iterator<Object[]> validContactsFromXml() throws IOException {
+        try (BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/contacts.xml")))) {
         String xml = "";
         String line = reader.readLine();
 
@@ -28,41 +29,53 @@ public class ContactCreationTests extends TestBase {
             xml += line;
             line = reader.readLine();
         }
-            XStream xstream = new XStream();
-            xstream.processAnnotations(ContactData.class);
-            List<ContactData> contacts = (List<ContactData>) xstream.fromXML(xml);
-            return contacts.stream().map((c) -> new Object[] {c}).collect(Collectors.toList()).iterator();
+        XStream xstream = new XStream();
+        xstream.processAnnotations(ContactData.class);
+        List<ContactData> contacts = (List<ContactData>) xstream.fromXML(xml);
+        return contacts.stream().map((c) -> new Object[]{c}).collect(Collectors.toList()).iterator();
+    }
+    }
+    @DataProvider
+    public Iterator<Object[]> validContactsFromJson() throws IOException {
+        try (BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/groups.json")))) {
+            String json = "";
+            String line = reader.readLine();
+            while (line != null) {
+                json += line;
+                line = reader.readLine();
+            }
+                Gson gson = new Gson();
+                List<ContactData> contacts = gson.fromJson(json, new TypeToken<List<ContactData>>() {
+                }.getType());
+                return contacts.stream().map((c) -> new Object[]{c}).collect(Collectors.toList()).iterator();
+            }
+        }
+
+        @Test(dataProvider = "validContactsFromJson")
+        public void testContactCreation (ContactData contact) throws Exception {
+            app.goTo().homePage();
+            Contacts before = app.contact().all();
+            app.contact().create(contact);
+            app.goTo().homePage();
+            assertThat(app.contact().count(), equalTo(before.size() + 1));
+            Contacts after = app.contact().all();
+            assertThat(after, equalTo(before.withAdded(contact.withId(after.stream().mapToInt((c) -> c.getId()).max().getAsInt()))));
+        }
+
+        @Test
+        public void testBadContactCreation () throws Exception {
+            app.goTo().homePage();
+            Contacts before = app.contact().all();
+            ContactData contact = new ContactData()
+                    .withFirstName("william'")
+                    .withLastName("Burroughs")
+                    .withGroup("name");
+            app.contact().create(contact);
+            app.goTo().homePage();
+            assertThat(app.contact().count(), equalTo(before.size()));
+            Contacts after = app.contact().all();
+            assertThat(after, equalTo(before));
+
+        }
 
     }
-
-    @Test(dataProvider = "validContacts")
-        public void testContactCreation(ContactData contact) throws Exception {
-        app.goTo().homePage();
-        Contacts before = app.contact().all();
-        app.contact().create(contact);
-        app.goTo().homePage();
-        assertThat(app.contact().count(), equalTo(before.size() + 1));
-        Contacts after = app.contact().all();
-        assertThat(after, equalTo(before.withAdded(contact.withId(after.stream().mapToInt((c) -> c.getId()).max().getAsInt()))));
-
-
-
-    }
-
-    @Test
-    public void testBadContactCreation() throws Exception {
-        app.goTo().homePage();
-        Contacts before = app.contact().all();
-        ContactData contact = new ContactData()
-                .withFirstName("william'")
-                .withLastName("Burroughs")
-                .withGroup("name");
-        app.contact().create(contact);
-        app.goTo().homePage();
-        assertThat(app.contact().count(), equalTo(before.size()));
-        Contacts after = app.contact().all();
-        assertThat(after, equalTo(before));
-
-    }
-
-}
