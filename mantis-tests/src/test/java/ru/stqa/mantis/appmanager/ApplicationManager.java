0c2d8f0b5673fccad917bbcfc8bef1e1ebe02969
svo216 <svo.hljott.216@gmail.com>
2019-08-27 19:46:25 +0500

    реализован тест на логин в mantis

--- mantis-tests/src/test/java/ru/stqa/mantis/appmanager/ApplicationManager.java
+++ mantis-tests/src/test/java/ru/stqa/mantis/appmanager/ApplicationManager.java
@@ -40,4 +40,11 @@ public class ApplicationManager {
             wd.quit();
         }
 
+        public HttpSession newSession() {
+        return new HttpSession(this);
+        }
+
+    public String getProperty(String key) {
+        return properties.getProperty(key);
+    }
 }
